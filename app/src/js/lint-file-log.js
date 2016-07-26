e45908bd110b9efbfda9551b73ba5f591ea11554
Brian Mock <brian@mockbrian.com>
2016-07-25 21:31:09 -0700

    Clickable file headers

--- app/src/js/lint-file-log.js
+++ app/src/js/lint-file-log.js
@@ -1,8 +1,24 @@
 const R = require('react').createElement;
 
+const backend = require('./backend');
+
+function openUrl(file) {
+  return 'javascript:;';
+}
+
+function openUrlHandler(editor, file, event) {
+  backend.open(editor, file, 1, 1);
+  event.preventDefault();
+}
+
 function LintFileLog(props) {
-  const {log} = props;
-  return R('div', {className: 'LogEntry LogEntryFile'}, log.file);
+  const {settings, log} = props;
+  const {editor} = settings;
+  const {file} = log;
+  const href = openUrl(file);
+  const onClick = openUrlHandler.bind(null, editor, file);
+  const className = 'LogEntry LogEntryFile';
+  return R('a', {href, onClick, className}, file);
 }
 
 module.exports = LintFileLog;
